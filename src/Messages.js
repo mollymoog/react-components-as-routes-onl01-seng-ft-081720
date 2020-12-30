8a8e9ab6f006aec3a76e8aeb01d5b4ef1cc71575
mollymoog <mmkover@gmail.com>
2020-12-30 15:15:52 -0500

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Messages extends React.Component{
+    render(){
+        return (
+        <ul>
+          <li>first message</li>
+        </ul>
+        )
+    }
+}
+
+export default Messages
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home';
+import About from './About';
+import Login from './Login';
+import Messages from './Messages';
+import Navbar from './Navbar';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 ReactDOM.render(
-  <Home />,
+  (<Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/messages" component={Messages} />
+    </div>
+  </Router>),
   document.getElementById('root')
 );
